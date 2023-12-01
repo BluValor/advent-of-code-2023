d8a60cfd0ef5c21323b53283ff3a618bc8d9bc20
BluValor <jazajma@gmail.com>
2023-12-01 22:45:15 +0100

    day 1 solution

---
+++ day_1_trebuchet/src/main.rs
@@ -0,0 +1,22 @@
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+mod part1;
+use part1::get_line_number as part1_get_line_number;
+
+mod part2;
+use part2::get_line_number as part2_get_line_number;
+
+fn main() -> io::Result<()> {
+
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    let result = reader.lines().fold(0, |acc, line| {
+        acc + part2_get_line_number(&line.unwrap())
+    });
+
+    println!("{}", result);
+
+    Ok(())
+}
---
+++ day_1_trebuchet/src/part1.rs
@@ -0,0 +1,34 @@
+const THRESHOLD: u32 = '9' as u32 + 1;
+
+// just a helper for engineering the solution
+macro_rules! print_char {
+    ($ch:literal) => {
+        println!(" ___ {}", $ch);
+        println!("(u32: {}, i32: {}) -> {}", $ch as u32, $ch as i32, $ch as i32 - THRESHOLD as i32);
+        println!("is number: {}", ($ch as u32) < (THRESHOLD as u32));
+        println!("(u8: {}, i8: {}) -> {}", $ch as u8, $ch as i8, $ch as i8 - THRESHOLD as i8);
+        println!("is number: {}", ($ch as u8) < (THRESHOLD as u8));
+        println!("");
+    };
+}
+
+// just a helper for engineering the solution
+#[allow(dead_code)]
+fn check_unicode() {
+    print_char!('0');
+    print_char!('9');
+    print_char!('a');
+    print_char!('z');
+    print_char!('A');
+    print_char!('Z');
+}
+
+fn is_number(c: char) -> bool {
+    (c as u32) < THRESHOLD 
+}
+
+pub fn get_line_number(line: &str) -> u32 {
+    let first_number = line.chars().find(|&c| is_number(c)).unwrap();
+    let last_number = line.chars().rev().find(|&c| is_number(c)).unwrap();
+    format!("{}{}", first_number, last_number).parse().unwrap()
+}
