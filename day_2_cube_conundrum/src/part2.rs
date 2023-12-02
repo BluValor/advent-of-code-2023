78bf2597ea794a5ceb50af2d38bc6a4b1437b4e3
BluValor <jazajma@gmail.com>
2023-12-02 13:55:45 +0100

    remove ugly ifs in part2

--- day_2_cube_conundrum/src/part2.rs
+++ day_2_cube_conundrum/src/part2.rs
@@ -3,15 +3,9 @@ use crate::common::{Draw, Game};
 pub fn get_game_number(game: Game, game_index: u32) -> u32 {
     let mut min_draw = Draw { r: 0, g: 0, b: 0 };
     game.draws.iter().for_each(|draw| {
-        if min_draw.r < draw.r {
-            min_draw.r = draw.r;
-        }
-        if min_draw.g < draw.g {
-            min_draw.g = draw.g;
-        }
-        if min_draw.b < draw.b {
-            min_draw.b = draw.b;
-        }
+        min_draw.r = u32::max(min_draw.r, draw.r);
+        min_draw.g = u32::max(min_draw.g, draw.g);
+        min_draw.b = u32::max(min_draw.b, draw.b);
     });
     min_draw.r * min_draw.g * min_draw.b
 }
